0867c2d676aa41944ebaf5a559dd7df3b344762d
Yernar <ernar30@gmail.com>
2025-05-28 19:08:32 +0500

    fix

--- MyDice/MyDice/Services/ProbabilityCalculator.cs
+++ MyDice/MyDice/Services/ProbabilityCalculator.cs
@@ -27,30 +27,6 @@ namespace MyDice.Services
 
             return (double)wins / total;
         }
-
-        public static double CalculateDrawChance(List<int> diceA, List<int> diceB)
-        {
-            if (diceA == null) throw new ArgumentNullException(nameof(diceA));
-            if (diceB == null) throw new ArgumentNullException(nameof(diceB));
-
-            int draws = 0;
-            int total = diceA.Count * diceB.Count;
-
-            foreach (var a in diceA)
-            {
-                draws += diceB.Count(b => a == b);
-            }
-
-            return (double)draws / total;
-        }
-
-        public static (double winChance, double drawChance, double loseChance) CalculateAllChances(List<int> diceA, List<int> diceB)
-        {
-            double winChance = CalculateWinChance(diceA, diceB);
-            double drawChance = CalculateDrawChance(diceA, diceB);
-            double loseChance = 1.0 - winChance - drawChance;
-
-            return (winChance, drawChance, loseChance);
-        }
+   
     }
 }
